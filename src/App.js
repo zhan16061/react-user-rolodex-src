0383d7556cca5c62529ae0e111a9718488dce40d
國展 <zhan16061@gmail.com>
2019-08-10 16:46:11 +0800

    first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './Component/CardList/CardList'
+import { SearchBox } from './Component/SearchBox/SearchBox'
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      users: []
+      users: [],
+      searchField: ''
     }
+    this.handleChange = this.handleChange.bind(this)
   }
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ users }))
+      .catch(error => console.error('Error:', error));
+  }
+
+  handleChange(e) {
+    this.setState({ searchField: e.target.value })
   }
   render() {
+    const { users, searchField } = this.state
+    const filterUsers = users.filter(user =>
+      user.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return (
       <div className="App">
-        <CardList users={this.state.users} />
+        <h1>User Roldex</h1>
+        <SearchBox placeholder='search user' handleChange={this.handleChange}/>
+        <CardList users={filterUsers} />
       </div>
     );
   }
--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Card.css'
 export const Card = props => {
   return (
-    <div class='card-container' >
+    <div className='card-container' >
       <img alt='user' src={`https://robohash.org/${props.user.id}?set=set2&size=180x180`} />
       <h1>{props.user.name}</h1>
       <p>{props.user.email}</p>
